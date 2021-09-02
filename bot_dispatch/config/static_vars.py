1028a61a4b4008f0fe740361f14722c4359e11b7
ettzzz <ertenzdya2005@gmail.com>
2021-09-02 10:18:01 +0800

    feat: split private_info, but anyway this is a private repo

--- bot_dispatch/config/static_vars.py
+++ bot_dispatch/config/static_vars.py
@@ -4,35 +4,15 @@ Created on Sat Apr 17 10:21:43 2021
 
 @author: ert
 """
+
 import os
 
+from private.private_info import EMPLOYEE_ROSTER, DQN_AGENT_HOST
+from private.private_info import pois
+
 ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-
-ME = '476917241'
-ANOTHER_ME = '1433361729'
-
-EMPLOYEE_ROSTER = {
-    'probius': {
-        'description': 'looking for a rent.',
-        'api_token': '781431817:AAEt1G2VaqMyu4OARKLnufp6eqBnHz6zORU',
-        'chat_id': ME
-        },
-    'blog_notify_bot': {
-        'description': 'used for ettzzz.me but now it\'s now used for active interactions.',
-        'api_token': '966629533:AAFJOn_J_EDEQZPcRftHdSEHHu38PtGxrPI',
-        'chat_id': ME
-        },
-    'jobsuchen_bot': {
-        'description': 'looking for a job.',
-        'api_token': '764684634:AAHmOxQfMXIHog8q9qaj8Ji2v8SzHJZmt7U',
-        'chat_id': ME
-        },
-    }
-
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'
 BASE_HEADERS = {
     "User-Agent": UA
 }
-
-DQN_AGENT_HOST = 'http://82.157.178.246:7704'
--- bot_dispatch/tg_operator/receptionist.py
+++ bot_dispatch/tg_operator/receptionist.py
@@ -17,6 +17,7 @@ from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 from config.static_vars import BASE_HEADERS, EMPLOYEE_ROSTER, ROOT, DQN_AGENT_HOST
+from config.static_vars import pois
 
 # Enable logging
 logging.basicConfig(
@@ -42,24 +43,26 @@ logger = logging.getLogger(__name__)
 #     update.message.reply_text('Help!')
 
 
-def baipiaov2ray(update: Update, _: CallbackContext): # _ is a must...
+def baipiaov2ray(update: Update, _: CallbackContext):  # _ is a must...
     today = datetime.datetime.today()
     yesterday = today + datetime.timedelta(-1)
     today_str = '%02d' % today.month + '%02d' % today.day
     yesterday_str = '%02d' % yesterday.month + '%02d' % yesterday.day
-    today_url = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{}.txt'.format(today_str)
-    yesterday_url = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{}.txt'.format(yesterday_str)
+    today_url = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{}.txt'.format(
+        today_str)
+    yesterday_url = 'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{}.txt'.format(
+        yesterday_str)
     try:
         rt = requests.get(
-            url = today_url,
-            headers = BASE_HEADERS,
-            timeout = 5
-            )
+            url=today_url,
+            headers=BASE_HEADERS,
+            timeout=5
+        )
         ry = requests.get(
-            url = yesterday_url,
-            headers = BASE_HEADERS,
-            timeout = 5
-            )
+            url=yesterday_url,
+            headers=BASE_HEADERS,
+            timeout=5
+        )
         if not rt.text.startswith('404'):
             text = '白嫖v2ray{}更新啦！'.format(today_str)
             link = today_url
@@ -82,21 +85,17 @@ def baipiaov2ray(update: Update, _: CallbackContext): # _ is a must...
 
 
 def isitgoingtorain(update: Update, _: CallbackContext):
-    pois = {
-        'home': {'lat':'39.819', 'lon': '116.371'},
-        'work': {'lat':'39.908', 'lon': '116.554'}
-    }
     base_url = 'https://isitgoingtorain.com/data/forecast.php'
     r_home = requests.get(
         base_url,
-        params = pois['home'],
-        headers = BASE_HEADERS
+        params=pois['home'],
+        headers=BASE_HEADERS
     )
 
     r_work = requests.get(
         base_url,
-        params = pois['work'],
-        headers = BASE_HEADERS
+        params=pois['work'],
+        headers=BASE_HEADERS
     )
 
     useful_str_home = re.findall('initForecast[(](.*?)[)];', r_home.text)
@@ -104,7 +103,7 @@ def isitgoingtorain(update: Update, _: CallbackContext):
 
     if len(useful_str_home) > 0 and len(useful_str_work) > 0:
         now = time.localtime()
-        max_func = lambda x: x['pP']
+        def max_func(x): return x['pP']
 
         forecast_home = json.loads(useful_str_home[0])
         forecast_work = json.loads(useful_str_work[0])
@@ -115,25 +114,25 @@ def isitgoingtorain(update: Update, _: CallbackContext):
         tomorrows_work = forecast_work['forecast'][1]['data']
 
         fresh = [
-            max(todays_home[7:9], key=max_func)['pP'], # morning today
-            max(todays_work[17:19], key=max_func)['pP'], # offwork today
-            max(todays_home[21:] + tomorrows_home[:7], key=max_func)['pP'], # night today
-            max(tomorrows_home[7:9], key=max_func)['pP'], # morning tomorrow
-            max(tomorrows_work[17:19], key=max_func)['pP'] # offwork tomorrow
-            ]
+            max(todays_home[7:9], key=max_func)['pP'],  # morning today
+            max(todays_work[17:19], key=max_func)['pP'],  # offwork today
+            max(todays_home[21:] + tomorrows_home[:7], key=max_func)['pP'],  # night today
+            max(tomorrows_home[7:9], key=max_func)['pP'],  # morning tomorrow
+            max(tomorrows_work[17:19], key=max_func)['pP']  # offwork tomorrow
+        ]
 
         if now.tm_hour <= 8:
             text = '今天上班下雨max p={}, 下班下雨max p={}, 晚上下雨max p={}。'.format(
                 fresh[0], fresh[1], fresh[2]
-                )
+            )
         elif now.tm_hour <= 19 and now.tm_hour > 8:
             text = '今天下班下雨max p={}, 晚上下雨max p={}, 明天上班下雨max p={}。'.format(
                 fresh[1], fresh[2], fresh[3]
-                )
+            )
         else:
             text = '今天睡觉下雨max p={}, 明天上班下雨max p={}, 明天下班下雨max p={}。'.format(
                 fresh[2], fresh[3], fresh[4]
-                )
+            )
     else:
         text = '淦 requests出错了，赶紧debug'
 
@@ -143,6 +142,7 @@ def isitgoingtorain(update: Update, _: CallbackContext):
 def stock_query_status(update, _):
     r = requests.get('{}/api_v1/query_status'.format(DQN_AGENT_HOST))
     update.message.reply_text(r.json()['msg'])
+
 
 def stock_reset_status(update, _):
     r = requests.get('{}/api_v1/reset_status'.format(DQN_AGENT_HOST))
