9a48e916b5df25ab75e7ac7f12f7630dc10c8266
ettzzz <ertenzdya2005@gmail.com>
2021-10-18 22:05:40 +0800

    fix: static vars in tgbot_python.sh

--- tgbot_python.sh
+++ tgbot_python.sh
@@ -1,7 +1,7 @@
 #！/bin/bash
 
 pid_file="pids.tgbot"
-conda_python_bin="/root/miniconda3/envs/django/bin/python3"
+conda_python_bin="/$USER/miniconda3/envs/django/bin/python3"
 
 kill_now(){
     cat $pid_file|xargs kill -9
@@ -13,14 +13,14 @@ start_new(){
 
 option=$1
 
-if [ $option == "reload" ]; then
+if [ $option == "--reload" ]; then
     kill_now
     start_new
     echo "reload done"
-elif [ $option == "stop" ]; then
+elif [ $option == "--stop" ]; then
     kill_now
     echo "stop done"
-elif [ $option == "start" ]; then
+elif [ $option == "--start" ]; then
     start_new
     echo "start done"
 else
