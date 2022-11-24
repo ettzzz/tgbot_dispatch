9eddaf36742dc5f41bd686bfd7a95ee033898882
ettzzz <ertenzdya2005@gmail.com>
2022-11-24 21:23:08 +0800

    feat: add weather_reminder

--- bot_apis/_manager.py
+++ bot_apis/_manager.py
@@ -10,7 +10,7 @@ from telegram.ext import CallbackContext, Updater, CommandHandler
 
 from configs.private_vars import BOT_INFO
 from .v2ray_update.nice_scrapper import call_nice_scrapper
-
+from .weather_query.weather_reminder import call_weather_reminder
 
 def echo(update: Update, _: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
@@ -27,6 +27,7 @@ def create_interactive_updater():
 
     dispatcher.add_handler(CommandHandler("hi", helloworld))
     dispatcher.add_handler(CommandHandler("v2ray", call_nice_scrapper))
+    dispatcher.add_handler(CommandHandler("wetter", call_weather_reminder))
 
     updater.start_polling()
     # updater.idle() ## only use this when you are on a IDE and have access to Internet
---
+++ bot_apis/weather_query/weather_reminder.py
@@ -0,0 +1,35 @@
+
+
+import requests
+from telegram.ext import CallbackContext
+from telegram import Update
+
+
+def weather_broadcast(citycode=101010300):
+    """
+    check citycode at http://api.help.bj.cn/api/CityCode.XLS
+    """
+    url = "http://api.help.bj.cn/apis/weather"
+    params = {"id": str(citycode)}
+    r = requests.get(url, params = params)
+    if r.status_code != 200:
+        return f"天气API网络错误 code {r.status_code}"
+    res = r.json()
+
+    today = res.get("today")
+    uptime = res.get("uptime")
+    temp = res.get("temp")
+    wd = res.get("wd")
+    wdforce = res.get("wdforce")
+    humidity = res.get("humidity")
+    aqi = res.get("aqi")
+    precipitation_24h = res.get("prcp24h")
+
+    text = f"{today} {uptime}实时天气：温度 {temp}°C，湿度 {humidity}，风速 {wd}{wdforce}，空气指数 {aqi}，24小时降雨量 {precipitation_24h}。"
+    return text
+
+
+def call_weather_reminder(update: Update, _: CallbackContext):  # _ is a must...
+    text = weather_broadcast() ## todo: how can we use variable?
+    html_text = f'<a href="">{text}</a>'
+    update.message.reply_html(html_text, disable_web_page_preview=True)
