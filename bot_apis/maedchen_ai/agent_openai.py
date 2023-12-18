3bc23c7c15533d2f5b6e462b1eb0111cb48e557f
ettzzz <39869906+ettzzz@users.noreply.github.com>
2023-12-18 03:05:39 +0000

    refactor: agent_openai.py

---
+++ bot_apis/maedchen_ai/agent_openai.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 11:23:55 2023
+
+@author: eee
+"""
+import os
+import json
+
+import requests
+
+from utils.datetime_tools import get_today_date
+
+class ChatGPTAgent:
+    def __init__(self):
+        # Setting the API key to use the OpenAI API
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        self.api_host = os.getenv("OPENAI_API_BASE")
+        self.knowledge_cutoff = os.getenv("KNOWLEDGE_CUTOFF") # 2021-09
+        
+        self.chat_url = f'{self.api_host}/v1/chat/completions'
+        self.headers = {
+            'Authorization': f"Bearer {self.api_key}",
+            'Content-Type': 'application/json',
+        }
+        self.system_prompt = f"""You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.
+            Knowledge cutoff: {self.knowledge_cutoff}
+            Current date: {get_today_date()}"""
+        self.init_prompt = "Hello! Good to see you there!"
+        
+        self.token_thresh = 8192
+        self.messages = []
+        
+    def chat(self, message, model):
+        self.messages.append({"role": "user", "content": message})
+        try:
+            data = {
+                "model": model,
+                "stream": False,
+                "messages": self.messages,
+                "temperature": 0.2,
+                "max_tokens": 2000,
+            }
+            d = json.dumps(data)
+            response = requests.post(self.chat_url, headers=self.headers, data=d, timeout=300)
+            content = response.json()["choices"][0]["message"]["content"]
+            total_tokens = response.json()["usage"]["total_tokens"]
+            if total_tokens >= self.token_thresh:
+                self.messages = self.messages[1:]
+            self.messages.append({"role": "assistant", "content": content})
+            return content
+        except Exception as e:
+            return f"ERROR: {e}"
+
+    def start(self, model):
+        self.messages = [{"role": "system", "content": self.system_prompt}]
+        return self.chat(self.init_prompt, model)
+
+
+if __name__ == "__main__":
+    c = ChatGPTAgent()
+    c.chat("hello there")
+    print(c.chat("can you show me how to write a recursive function in python?"))
--- bot_apis/maedchen_ai/apis.py
+++ bot_apis/maedchen_ai/apis.py
@@ -5,17 +5,20 @@ Created on Fri Mar 10 15:30:13 2023
 
 @author: eee
 """
+import os
 
 from telegram.ext import ContextTypes
 from telegram import Update
-from bot_apis.maedchen_ai.agent_sb import ChatGPTAgent
+from bot_apis.maedchen_ai.agent_openai import ChatGPTAgent
 
 agent = ChatGPTAgent()
 
 GPT3_5_ROUTES, GPT4_ROUTES = range(2)  # Stages 0, 1
+GPT3_5_MODEL = os.getenv("GPT3_5_MODEL")
+GPT4_MODEL = os.getenv("GPT4_MODEL")
 
 async def call_ai_reboot(update, context):
-    from_chatgpt = agent.start()
+    from_chatgpt = agent.start(model=GPT3_5_MODEL)
     try:
         await update.message.reply_text(from_chatgpt, parse_mode="Markdown")
     except:
@@ -25,7 +28,7 @@ async def call_ai_reboot(update, context):
 
 async def call_ai_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     from_user = update.message.text
-    from_chatgpt = agent.chat(from_user)
+    from_chatgpt = agent.chat(from_user, model=GPT3_5_MODEL)
     try:
         await update.message.reply_text(from_chatgpt, parse_mode="Markdown")
     except:
@@ -34,7 +37,7 @@ async def call_ai_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def call_ai_reboot4(update, context):
-    from_chatgpt = agent.start(model="gpt-4")
+    from_chatgpt = agent.start(model=GPT4_MODEL)
     try:
         await update.message.reply_text(from_chatgpt, parse_mode="Markdown")
     except:
@@ -44,7 +47,7 @@ async def call_ai_reboot4(update, context):
 
 async def call_ai_chat4(update: Update, context: ContextTypes.DEFAULT_TYPE):
     from_user = update.message.text
-    from_chatgpt = agent.chat(from_user, model="gpt-4")
+    from_chatgpt = agent.chat(from_user, model=GPT4_MODEL)
     try:
         await update.message.reply_text(from_chatgpt, parse_mode="Markdown")
     except:
